303d1b2897cd19a8a197a9ecf8fde3d66bf2aa3a
fanshijun1 <1216323692@qq.com>
2020-01-29 08:19:40 +0800

    add flyway:migration support

--- src/main/java/life/majiang/community/Controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/Controller/AuthorizeController.java
@@ -7,12 +7,13 @@ import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.origin.SystemEnvironmentOrigin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -31,7 +32,8 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code, @RequestParam(name="state") String state, HttpServletRequest request){
+    public String callback(@RequestParam(name="code") String code, @RequestParam(name="state") String state,
+                           HttpServletRequest request, HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -46,15 +48,17 @@ public class AuthorizeController {
         if(githubuser!=null){
             //登录成功写cookie和session
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubuser.getName());
             user.setAccountId(String.valueOf(githubuser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
 
             userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
 
-            request.getSession().setAttribute("user",user);
+            //request.getSession().setAttribute("user",user);
 
             return "redirect:/";
         }else{
